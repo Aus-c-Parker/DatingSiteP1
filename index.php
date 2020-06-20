ce06130e0fc9814a0d6faf1d98444112392b454e
Austin Parker <aparker24@mail.greenriver.edu>
2020-06-20 06:54:45 -0400

    Built basic file structure and install Composer.

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+
+//Turn on error reporting
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+
+session_start();
+
+//Require the autoload file
+require_once('vendor/autoload.php');
+
+//Instantiate the F3 Base class
+$f3 = Base::instance();
+
+//Define a default route
+$f3 -> route('GET /', function() {
+    $view = new Template();
+    echo $view->render('views/home.html');
+});
+
+//Run fat free
+$f3 -> run();
